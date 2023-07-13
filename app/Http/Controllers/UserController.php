30d6150ef1ae54ea64374e48f30a3026d3efb0d9
SrZitu <zitu094@gmail.com>
2023-07-13 22:50:30 +0600

    userLogin

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,34 +2,46 @@
 
 namespace App\Http\Controllers;
 
+use App\Helper\JWTToken;
+
 use App\Models\User;
+
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    public function UserLogin()
+    public function UserLogin(Request $request)
     {
+        $result = User::where($request->input())->count();
+        if ($result == 1) {
+            $token = JWTToken::CreateJWTToken($request->input('email'));
+            return response()->json(['data' => $token, 'msg' => 'success']);
+
+        } else {
+
+            return response()->json(['data' => 'unauthorized', 'msg' => 'success']);
+        }
     }
+
     public function UserRegistration(Request $request)
     {
         return User::create($request->input());
     }
+
     public function SendOtpToEmail()
     {
-
     }
+
     public function VerifyOtp()
     {
-
     }
     public function SetPassword()
     {
-
     }
 
     //after login
 
-    public function ProfileUpdate(){
-
+    public function ProfileUpdate()
+    {
     }
 }
